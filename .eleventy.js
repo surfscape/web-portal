958cbe1ff83a02e8e9a3baf1eb898baef92b2275
ReduxFlakes <87225951+ReduxFlakes@users.noreply.github.com>
2023-01-28 21:09:59 +0000

    Test deploy

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets/");
+  eleventyConfig.addPassthroughCopy("src/css/");
+  eleventyConfig.addWatchTarget("src/css");
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      layouts: "layouts",
+      output: "_site",
+      data: "data",
+    },
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  };
+};
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# eversoul
-My personal website
+# [eversoul](https://eversoul.neocities.org)
+
+eversoul is my personal website where I host my projects, have my blog and eveything that I want to share to the world.
+
+The design and layout is very reminiscent from the 90's an 2000's blog websites; a big header, main content and a sidebar.
+
+## Credits:
+
+SSG: 11ty
+Host: neocities
