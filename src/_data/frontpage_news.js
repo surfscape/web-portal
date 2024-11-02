1199a11460d088023e00eed9b1a74627b47e5047
ReduxFlakes <tiagompca@proton.me>
2024-11-02 23:42:54 +0000

    add: news system to the sidebar

---
+++ src/_data/frontpage_news.js
@@ -0,0 +1,10 @@
+export default {
+  quickies: [
+    {
+      title: "Back up again!",
+      date: "02/11/2024",
+      content:
+        "We are happy to announce that SurfScape is finally back online! Most of the portal has been rewritten with accessibility and content in mind so welcome.",
+    },
+  ],
+};
