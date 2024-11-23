c4fa6240731f0fa5d1104dc869d86874dc3aea64
ReduxFlakes <tiagompca@proton.me>
2024-11-23 17:24:14 +0000

    fix: wrong url in ecosystem.js

--- src/_data/ecosystem.js
+++ src/_data/ecosystem.js
@@ -3,7 +3,7 @@ export default [
     component: "SurfScape Directory",
     description:
       "SurfScape public website directory available on the web portal",
-    privacy: "/privacy-policy/",
+    privacy: "/privacy/",
     website: "/library/directory/",
   },
 ];
